22245906948bfe5b43b63f1f51fe819bff548078
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-06-09 14:58:58 +0200

    reduced all ingredients to one array and added validation to check if there are ingredients

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -4,7 +4,7 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient"
 
 const burger = (props) => {
 	// Loop through given object
-	const transformedIngredients = Object.keys(props.ingredients) // Loop through ingredients object and pass in the keys (the name of the ingredient)
+	let transformedIngredients = Object.keys(props.ingredients) // Loop through ingredients object and pass in the keys (the name of the ingredient)
 		.map((igKey) => {
 			//map through all the names.
 			// return console.log(igKey)
@@ -12,9 +12,16 @@ const burger = (props) => {
 				.map((_, i) => {
 					//_ being all the arrays and i being the amount of times the name appears in here
 					return <BurgerIngredient key={igKey + i} type={igKey} /> // return burger ingredient with igkey being the name and the amount of times it appears int here, so if salad returns (i) times, then return it that many times
-				}) // [' .. ']
+				}) //now youll get an array for each name (igkey) and then youll get the length of the array being the amount (i) of times 1 (igkey) comes in.
 		})
+		.reduce((arr, el) => {
+			return arr.concat(el) //loops through all values, pulls out all the elemnts and adds it into 1 arr
+		}, []) //reduce the array, transforms an array into something else, functions: previous value and current value
 
+	//if transformedingredients length is 0 give message
+	if (transformedIngredients.length === 0) {
+		transformedIngredients = <p>Please start adding ingredients!</p>
+	}
 	console.log(transformedIngredients)
 	return (
 		<div className={classes.Burger}>
