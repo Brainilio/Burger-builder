ee2bf5a83f8733ff08c79259cb8707aa82369788
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-07-05 13:39:14 +0200

    added conditional classes for backdrop & sidenavigation

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -1,15 +1,31 @@
-import React from "react"
+import React, { Component } from "react"
 import classes from "./Layout.module.css"
 import Toolbar from "../Navigation/Toolbar/Toolbar"
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer"
 
-const layout = (props) => (
+class Layout extends Component {
 	// Wrap the content in a higher order component
-	<>
-		<Toolbar />
-		<SideDrawer />
-		<main className={classes.Content}>{props.children}</main>
-	</>
-)
 
-export default layout
+	state = {
+		showSideDrawer: true,
+	}
+	//SIDEDRAWERCLOSEDHANDLER
+	SideDrawerClosedHandler = () => {
+		this.setState({ showSideDrawer: false })
+	}
+	render() {
+		return (
+			<>
+				<Toolbar />
+				<SideDrawer
+					//side drawer open or not
+					open={this.state.showSideDrawer}
+					closed={this.SideDrawerClosedHandler}
+				/>	
+				<main className={classes.Content}>{this.props.children}</main>
+			</>
+		)
+	}
+}
+
+export default Layout
