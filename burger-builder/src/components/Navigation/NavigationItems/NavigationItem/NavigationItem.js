89f4e5004a777033299dda601bc9153cf272ee9a
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-07-05 12:27:01 +0200

    added navigation links in toolbar

--- burger-builder/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ burger-builder/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -2,7 +2,13 @@ import React from "react"
 import classes from "./NavigationItem.module.css"
 
 const NavigationItem = (props) => {
-	return <div></div>
+	return (
+		<li className={classes.NavigationItem}>
+			<a href={props.link} className={props.active ? classes.active : null}>
+				{props.children}
+			</a>
+		</li>
+	)
 }
 
 export default NavigationItem
