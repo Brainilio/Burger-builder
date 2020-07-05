89327f86ebbb0bc7e9284a2e3d9176d4ce6d17bd
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-07-05 14:20:50 +0200

    ordersummary in classbased component for lifecycle method.

--- burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,36 +1,40 @@
 import React from "react"
 import Button from "../../UI/Button/Button"
+import { Component } from "react"
 
-const OrderSummary = (props) => {
-	// take ingreidnets an use object.keys to transform into array of keys: value
-	const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
+class OrderSummary extends Component {
+
+
+	render() {
+		// take ingreidnets an use object.keys to transform into array of keys: value
+		const ingredientSummary = Object.keys(this.props.ingredients).map((igKey) => {
+			return (
+				<li key={igKey}>
+					{/* igkey = the key and props.ingredients[key] reads the value */}
+					<span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
+					{this.props.ingredients[igKey]}
+				</li>
+			)
+		})
+
 		return (
-			<li key={igKey}>
-				{/* igkey = the key and props.ingredients[key] reads the value */}
-				<span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
-				{props.ingredients[igKey]}
-			</li>
+			<>
+				<h3>Your Order</h3>
+				<p>Your selected ingredients:</p>
+				<ul>{ingredientSummary}</ul>
+				<p>
+					<strong>Total Price: ${this.props.price.toFixed(2)}</strong>
+				</p>
+				<p>Continue to Checkout?</p>
+				<Button btnType="Danger" clicked={this.props.cancelClick}>
+					CANCEL
+				</Button>
+				<Button btnType="Success" clicked={this.props.continueClick}>
+					CHECKOUT
+				</Button>
+			</>
 		)
-	})
-	console.log(ingredientSummary)
-
-	return (
-		<>
-			<h3>Your Order</h3>
-			<p>Your selected ingredients:</p>
-			<ul>{ingredientSummary}</ul>
-			<p>
-				<strong>Total Price: ${props.price.toFixed(2)}</strong>
-			</p>
-			<p>Continue to Checkout?</p>
-			<Button btnType='Danger' clicked={props.cancelClick}>
-				CANCEL
-			</Button>
-			<Button btnType='Success' clicked={props.continueClick}>
-				CHECKOUT
-			</Button>
-		</>
-	)
+	}
 }
 
 export default OrderSummary
