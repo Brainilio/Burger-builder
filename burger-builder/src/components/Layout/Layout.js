da90c30f7885ccb77b46da0a4424870175ef60bd
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-07-05 12:38:42 +0200

    Sidedrawer complete TODO: Responsive adjustments

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from "react"
 import classes from "./Layout.module.css"
 import Toolbar from "../Navigation/Toolbar/Toolbar"
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer"
 
 const layout = (props) => (
 	// Wrap the content in a higher order component
 	<>
 		<Toolbar />
+		<SideDrawer />
 		<main className={classes.Content}>{props.children}</main>
 	</>
 )
