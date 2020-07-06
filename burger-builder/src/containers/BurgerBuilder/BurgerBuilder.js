30b1aca9dd9bcf728f6a45228a776d2d9bb0ec35
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-07-06 18:49:46 +0200

    Made a post request with body for sending burgeringredients to firebase.

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,7 @@ import Burger from "../../components/Burger/Burger"
 import BuildControls from "../../components/BuildControls/BuildControls"
 import Modal from "../../components/UI/Modal/Modal"
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary"
+import axios from "../../axios-orders"
 
 // Price for each ingredient
 const INGREDIENT_PRICES = {
@@ -80,8 +81,30 @@ class BurgerBuilder extends Component {
 		})
 	}
 
+	// HTTP request
 	purchaseContinueHandler = () => {
-		alert("You Continue!")
+		// alert("You Continue!")
+
+		//object that i want to send to the server
+		const order = {
+			ingredients: this.state.ingredients,
+			price: this.state.totalPrice,
+			customer: {
+				name: "Brainilio",
+				address: {
+					city: "San Jose",
+					country: "USA",
+				},
+				email: "BrainilioIR@gmail.com",
+			},
+			deliveryMethod: "fastest",
+		}
+
+		//.json for firebase
+		axios
+			.post("/orders.json", order)
+			.then((response) => console.log(response))
+			.catch((error) => console.log(error))
 	}
 
 	render() {
