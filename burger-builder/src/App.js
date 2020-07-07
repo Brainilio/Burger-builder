3628d3b3e78c088c2f06f2738168766c7825cef0
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-07-07 21:57:01 +0200

    cleanup testing

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -4,21 +4,12 @@ import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder"
 import { Component } from "react"
 
 class App extends Component {
-	// see if errorboundary unmounts
-	state = {
-		show: true,
-	}
-
-	// componentDidMount() {
-	// 	setTimeout(() => {
-	// 		this.setState({ show: false })
-	// 	}, 5000)
-	// }
-
 	render() {
 		return (
 			<div>
-				<Layout>{this.state.show ? <BurgerBuilder /> : null}</Layout>
+				<Layout>
+					<BurgerBuilder />{" "}
+				</Layout>
 			</div>
 		)
 	}
