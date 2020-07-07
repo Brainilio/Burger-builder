aff2e3f080b1a3e3f78388c0e48c74e1bfff2eaa
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-07-07 21:56:32 +0200

    remove interceptors when unmounting component

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,15 +1,27 @@
 import React from "react"
 import Layout from "./hoc/Layout/Layout"
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder"
+import { Component } from "react"
 
-function App() {
-	return (
-		<div>
-			<Layout>
-				<BurgerBuilder />
-			</Layout>
-		</div>
-	)
+class App extends Component {
+	// see if errorboundary unmounts
+	state = {
+		show: true,
+	}
+
+	// componentDidMount() {
+	// 	setTimeout(() => {
+	// 		this.setState({ show: false })
+	// 	}, 5000)
+	// }
+
+	render() {
+		return (
+			<div>
+				<Layout>{this.state.show ? <BurgerBuilder /> : null}</Layout>
+			</div>
+		)
+	}
 }
 
 export default App
--- burger-builder/src/hoc/ErrorHandler/ErrorHandler.js
+++ burger-builder/src/hoc/ErrorHandler/ErrorHandler.js
@@ -8,17 +8,27 @@ const withErrorHandler = (WrappedComponent, axios) => {
 			error: null,
 		}
 
+		//add interceptors
 		componentWillMount() {
-			axios.interceptors.request.use((req) => {
+			this.reqInterceptor = axios.interceptors.request.use((req) => {
 				this.setState({ error: null })
 				return req
 			})
-			axios.interceptors.response.use(
+			this.resInterceptor = axios.interceptors.response.use(
 				(res) => res,
 				(error) => {
 					this.setState({ error: error })
 				}
 			)
+		}
+
+		//get rid of interceptors
+		componentWillUnmount() {
+			console.log("Will unmount", this.reqInterceptor, this.resInterceptor)
+
+			//eject the interceptors
+			axios.interceptors.request.eject(this.reqInterceptor)
+			axios.interceptors.response.eject(this.resInterceptor)
 		}
 
 		errorConfirmedHandler = () => {
