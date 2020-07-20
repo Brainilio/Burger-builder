d2fdad5294eed71f44f6880d2e3aecb1819344b5
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-07-20 11:42:39 +0200

    css change

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -2,13 +2,15 @@ import React from "react"
 import Layout from "./hoc/Layout/Layout"
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder"
 import { Component } from "react"
+import Checkout from "./containers/Checkout/Checkout"
 
 class App extends Component {
 	render() {
 		return (
 			<div>
 				<Layout>
-					<BurgerBuilder />{" "}
+					<BurgerBuilder />
+					<Checkout />
 				</Layout>
 			</div>
 		)
