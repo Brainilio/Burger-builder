6a2ab1c8538d76dfdb0ef919224433a426fe962a
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-07-27 09:59:01 +0200

    SET Up reducer methods

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -28,16 +28,16 @@ class BurgerBuilder extends Component {
 
 	componentDidMount() {
 		console.log(this.props)
-		axios
-			.get(
-				"https://react-my-burger-builder-d060b.firebaseio.com/ingredients.json"
-			)
-			.then((response) => {
-				this.setState({ ingredients: response.data })
-			})
-			.catch((error) => {
-				this.setState({ error: true })
-			})
+		// axios
+		// 	.get(
+		// 		"https://react-my-burger-builder-d060b.firebaseio.com/ingredients.json"
+		// 	)
+		// 	.then((response) => {
+		// 		this.setState({ ingredients: response.data })
+		// 	})
+		// 	.catch((error) => {
+		// 		this.setState({ error: true })
+		// 	})
 	}
 
 	updatePurchaseState = (ingredients) => {
--- burger-builder/src/store/reducer.js
+++ burger-builder/src/store/reducer.js
@@ -10,7 +10,12 @@ const INGREDIENT_PRICES = {
 
 //initial state for ingredients
 const initialState = {
-	ingredients: null,
+	ingredients: {
+		salad: 0,
+		bacon: 0,
+		cheese: 0,
+		meat: 0,
+	},
 	totalPrice: 4,
 }
 
@@ -19,10 +24,19 @@ const reducer = (state = initialState, action) => {
 		case actionTypes.ADD_INGREDIENT:
 			return {
 				...state,
+				ingredients: {
+					...state.ingredients,
+					//override object key
+					[action.ingredientName]: state.ingredients[action.ingredientName] + 1,
+				},
 			}
 		case actionTypes.DELETE_INGREDIENT:
 			return {
 				...state,
+				ingredients: {
+					...state.ingredients,
+					[action.ingredientName]: state.ingredients[action.ingredientName] - 1,
+				},
 			}
 		default:
 			return state
