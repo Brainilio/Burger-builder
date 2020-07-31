216859b5e04bb718e1a472d2cdc4409b1d3260dc
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-07-31 17:09:17 +0200

    orderjs

--- burger-builder/src/store/reducers/order.js
+++ burger-builder/src/store/reducers/order.js
@@ -14,10 +14,8 @@ const reducer = (state = initialState, action) => {
 		case actionTypes.PURCHASE_BURGER_START:
 			return updateObject(state, { loading: true })
 		case actionTypes.PURCHASE_BURGER_SUCCESS:
-			const newOrder = {
-				...action.orderData,
-				id: action.orderId,
-			}
+			const newOrder = updateObject(action.orderData, { id: action.orderId })
+
 			return updateObject(state, {
 				loading: false,
 				purchased: true,
