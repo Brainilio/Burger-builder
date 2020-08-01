4817c073ab1f97724defebfc0690b9e9da93612d
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-08-01 13:39:23 +0200

    connected redux to auth.js

--- burger-builder/src/containers/Auth/Auth.js
+++ burger-builder/src/containers/Auth/Auth.js
@@ -2,6 +2,8 @@ import React, { Component } from "react"
 import Input from "../../components/UI/Input/Input"
 import Button from "../../components/UI/Button/Button"
 import classes from "./Auth.module.css"
+import * as actions from "../../store/actions/index"
+import { connect } from "react-redux"
 
 class Auth extends Component {
 	//local state for form
@@ -116,4 +118,10 @@ class Auth extends Component {
 	}
 }
 
-export default Auth
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onAuth: (email, password) => dispatch(actions.auth(email, password)),
+	}
+}
+
+export default connect(null, mapDispatchToProps)(Auth)
