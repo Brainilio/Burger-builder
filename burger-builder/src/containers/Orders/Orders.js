33de160537e1b0df23f7ff68a2bc6db623f7647f
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-08-06 09:20:44 +0200

    orders done

--- burger-builder/src/containers/Orders/Orders.js
+++ burger-builder/src/containers/Orders/Orders.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react"
+import React, { useEffect } from "react"
 import Order from "../../components/Order/Order"
 import Spinner from "../../components/UI/Spinner/Spinner"
 import axios from "../../axios-orders"
@@ -6,29 +6,37 @@ import withErrorHandler from "../../hoc/ErrorHandler/ErrorHandler"
 import * as actions from "../../store/actions/index"
 import { connect } from "react-redux"
 
-class Orders extends Component {
-	componentDidMount() {
-		this.props.onFetchOrders(this.props.token, this.props.userId)
+const Orders = (props) => {
+	useEffect(() => {
+		props.onFetchOrders(props.token, props.userId)
+	}, [])
+
+	let orders = <Spinner />
+	if (!props.loading) {
+		orders = props.orders.map((order) => (
+			<Order
+				key={order.id}
+				ingredients={order.ingredients}
+				price={order.price}
+			/>
+		))
 	}
 
-	render() {
-		let orders = <Spinner />
-		if (!this.props.loading) {
-			orders = this.props.orders.map((order) => (
-				<Order
-					key={order.id}
-					ingredients={order.ingredients}
-					price={order.price}
-				/>
-			))
-		}
-		return (
+	return (
+		<>
 			<div>
-				<h1 style={{ textAlign: "center", fontSize: "25px" }}>Your orders:</h1>
+				<h1
+					style={{
+						textAlign: "center",
+						fontSize: "25px",
+					}}
+				>
+					Your orders:
+				</h1>
 				{orders}
 			</div>
-		)
-	}
+		</>
+	)
 }
 
 const mapStateToProps = (state) => {
