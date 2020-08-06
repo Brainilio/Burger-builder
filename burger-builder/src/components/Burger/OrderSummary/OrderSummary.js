aa04fb841db71fa3e2446551fa472428542e2124
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-08-06 10:07:11 +0200

    Modal and ordersummary updated

--- burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -2,41 +2,35 @@ import React from "react"
 import Button from "../../UI/Button/Button"
 import { Component } from "react"
 
-class OrderSummary extends Component {
-	// Log whether it will update.
-
-	render() {
-		// take ingreidnets an use object.keys to transform into array of keys: value
-		const ingredientSummary = Object.keys(this.props.ingredients).map(
-			(igKey) => {
-				return (
-					<li key={igKey}>
-						{/* igkey = the key and props.ingredients[key] reads the value */}
-						<span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
-						{this.props.ingredients[igKey]}
-					</li>
-				)
-			}
-		)
-
+const OrderSummary = (props) => {
+	// take ingreidnets an use object.keys to transform into array of keys: value
+	const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
 		return (
-			<>
-				<h3>Your Order</h3>
-				<p>Your selected ingredients:</p>
-				<ul>{ingredientSummary}</ul>
-				<p>
-					<strong>Total Price: ${this.props.price.toFixed(2)}</strong>
-				</p>
-				<p>Continue to Checkout?</p>
-				<Button btnType="Danger" clicked={this.props.cancelClick}>
-					CANCEL
-				</Button>
-				<Button btnType="Success" clicked={this.props.continueClick}>
-					CHECKOUT
-				</Button>
-			</>
+			<li key={igKey}>
+				{/* igkey = the key and props.ingredients[key] reads the value */}
+				<span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
+				{props.ingredients[igKey]}
+			</li>
 		)
-	}
+	})
+
+	return (
+		<>
+			<h3>Your Order</h3>
+			<p>Your selected ingredients:</p>
+			<ul>{ingredientSummary}</ul>
+			<p>
+				<strong>Total Price: ${props.price.toFixed(2)}</strong>
+			</p>
+			<p>Continue to Checkout?</p>
+			<Button btnType="Danger" clicked={props.cancelClick}>
+				CANCEL
+			</Button>
+			<Button btnType="Success" clicked={props.continueClick}>
+				CHECKOUT
+			</Button>
+		</>
+	)
 }
 
 export default OrderSummary
--- burger-builder/src/components/UI/Modal/Modal.js
+++ burger-builder/src/components/UI/Modal/Modal.js
@@ -1,36 +1,39 @@
 import React from "react"
 import classes from "./Modal.module.css"
 import Backdrop from "../Backdrop/Backdrop"
-import { Component } from "react"
 
-class Modal extends Component {
+const Modal = (props) => {
 	//if show == true
-	shouldComponentUpdate(nextProps, nextState) {
-		//returns true if next props is not the same as this.props.show.
-		return (
-			nextProps.show !== this.props.show ||
-			nextProps.children !== this.props.children
-		)
-	}
+	// shouldComponentUpdate(nextProps, nextState) {
+	// 	//returns true if next props is not the same as this.props.show.
+	// 	return (
+	// 		nextProps.show !== this.props.show ||
+	// 		nextProps.children !== this.props.children
+	// 	)
+	// }
 
-	render() {
-		return (
-			<>
-				<Backdrop show={this.props.show} clicked={this.props.modalClosed} />
-				<div
-					style={{
-						// If show is true, show the modal, if not then move it outside of the screen
-						// also add the opacity to it, this is an object so you can take advantage of it
-						transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
-						opacity: this.props.show ? "1" : "0",
-					}}
-					className={classes.Modal}
-				>
-					{this.props.children}
-				</div>
-			</>
-		)
-	}
+	return (
+		<>
+			<Backdrop show={props.show} clicked={props.modalClosed} />
+			<div
+				style={{
+					// If show is true, show the modal, if not then move it outside of the screen
+					// also add the opacity to it, this is an object so you can take advantage of it
+					transform: props.show ? "translateY(0)" : "translateY(-100vh)",
+					opacity: props.show ? "1" : "0",
+				}}
+				className={classes.Modal}
+			>
+				{props.children}
+			</div>
+		</>
+	)
 }
 
-export default Modal
+//optimized with memo, only updates if previous props is the same as the current props
+export default React.memo(
+	Modal,
+	(prevProps, nextProps) =>
+		nextProps.show === prevProps.show &&
+		nextProps.children === prevProps.children
+)
