8f8c3ba9b0e390e5e7e0decd2b4706d5cfe6bac5
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-08-06 09:07:11 +0200

    layout changed

--- burger-builder/src/hoc/Layout/Layout.js
+++ burger-builder/src/hoc/Layout/Layout.js
@@ -1,38 +1,30 @@
-import React, { Component } from "react"
+import React, { useState } from "react"
 import { connect } from "react-redux"
 import classes from "./Layout.module.css"
 import Toolbar from "../../components/Navigation/Toolbar/Toolbar"
 import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer"
 
-class Layout extends Component {
+const Layout = (props) => {
 	// Wrap the content in a higher order component
+	const [showSideDrawer, setShowSideDrawer] = useState(false)
 
-	state = {
-		showSideDrawer: false,
-	}
 	//SIDEDRAWERCLOSEDHANDLER
-	SideDrawerHandler = () => {
-		this.setState((prevState) => {
-			return { showSideDrawer: !prevState.showSideDrawer }
-		})
+	const SideDrawerHandler = () => {
+		setShowSideDrawer((prevState) => !prevState)
 	}
-	render() {
-		return (
-			<>
-				<Toolbar
-					isAuth={this.props.isAuthenticated}
-					handleSide={this.SideDrawerHandler}
-				/>
-				<SideDrawer
-					//side drawer open or not
-					isAuth={this.props.isAuthenticated}
-					open={this.state.showSideDrawer}
-					closed={this.SideDrawerHandler}
-				/>
-				<main className={classes.Content}>{this.props.children}</main>
-			</>
-		)
-	}
+
+	return (
+		<>
+			<Toolbar isAuth={props.isAuthenticated} handleSide={SideDrawerHandler} />
+			<SideDrawer
+				//side drawer open or not
+				isAuth={props.isAuthenticated}
+				open={showSideDrawer}
+				closed={SideDrawerHandler}
+			/>
+			<main className={classes.Content}>{props.children}</main>
+		</>
+	)
 }
 
 const mapStateToProps = (state) => {
